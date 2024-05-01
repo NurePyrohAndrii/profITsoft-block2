bb7dddb330f74fb64e0f6ba430d0655137104e13
Andrii Pyroh <andrewpyrih@gmail.com>
2024-05-01 16:01:42 +0300

    Performed project init wiith required dependencies

---
+++ block2/src/test/java/com/profIITsoft/block2/Block2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profIITsoft.block2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
